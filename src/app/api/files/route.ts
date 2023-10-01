388ba1a41c883a95977f51f0b4bae3b5ed9abf97
Ariel Weinberger <weinberger.ariel@gmail.com>
2023-10-01 14:43:36 +0200

    create vector store index from PDF document

--- src/app/api/files/route.ts
+++ src/app/api/files/route.ts
@@ -1,11 +1,32 @@
 import { NextResponse } from "next/server";
 import pdf from "@cyber2024/pdf-parse-fixed";
+import { createOrReadVectorStoreIndex } from "@/lib/vector-store";
 
 export async function POST(req: Request) {
-  const formData = await req.formData();
-  const file = formData.get("file") as File;
-  const fileContents = await file.arrayBuffer();
-  const parsedPdf = await pdf(Buffer.from(fileContents));
+  try {
+    const formData = await req.formData();
+    const file = formData.get("file") as File;
+    const fileContents = await file.arrayBuffer();
+    const parsedPdf = await pdf(Buffer.from(fileContents));
 
-  
+    await createOrReadVectorStoreIndex(parsedPdf.text);
+
+    return NextResponse.json(
+      {
+        message: "File Uploaded",
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
---
+++ src/lib/vector-store.ts
@@ -0,0 +1,31 @@
+import {
+  Document,
+  VectorStoreIndex,
+  serviceContextFromDefaults,
+  storageContextFromDefaults,
+} from "llamaindex";
+
+export async function createOrReadVectorStoreIndex(
+  pdfText: string,
+): Promise<VectorStoreIndex> {
+  const document = new Document({ text: pdfText});
+  
+  const storageContext = await storageContextFromDefaults({
+    persistDir: "./storage",
+  });
+
+  const serviceContext = serviceContextFromDefaults({
+    chunkSize: 2000,
+    chunkOverlap: 500,
+  });
+
+  console.log("Creating index");
+
+  const index = await VectorStoreIndex.fromDocuments([document], {
+    storageContext,
+    serviceContext,
+  });
+
+  console.log("Index created");
+  return index;
+}
