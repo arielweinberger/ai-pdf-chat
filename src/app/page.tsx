552c81eadaf8c62bb3d0cc170eb2069b48adb313
Ariel Weinberger <weinberger.ariel@gmail.com>
2023-10-01 14:24:33 +0200

    use next/navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 export default function Home() {
-  return (window.location.href = "/upload");
+  const router = useRouter();
+  router.push("/upload");
 }
