6687d74555d3b533c840f67d66429d71d9499c8f
Ariel Weinberger <weinberger.ariel@gmail.com>
2023-10-01 21:38:03 +0200

    retrieve matching knowledge from vector store and provide to openai

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,4 +1,6 @@
+import { createOrReadVectorStoreIndex } from "@/lib/vector-store";
 import { OpenAIStream, StreamingTextResponse } from "ai";
+import { MetadataMode } from "llamaindex";
 import OpenAI from "openai";
 
 const openai = new OpenAI();
@@ -6,10 +8,37 @@ const openai = new OpenAI();
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
+  const systemMessage = {
+    role: "system",
+    content: "You are a helpful AI assistant",
+  };
+
+  const latestMessage = messages[messages.length - 1];
+  const index = await createOrReadVectorStoreIndex();
+
+  const retriever = index.asRetriever();
+  retriever.similarityTopK = 1;
+
+  const [matchingNode] = await retriever.retrieve(latestMessage.content);
+
+  if (matchingNode.score > 0.8) {
+    const knowledge = matchingNode.node.getContent(MetadataMode.NONE);
+
+    systemMessage.content = `
+      You are a helpful AI assistant. Your knowledge is enriched by this document:
+      ---
+      ${knowledge}
+      ---
+      When possible, explain the reasoning for your responses based on this knowledge.
+    `;
+
+    console.log("knowledge", knowledge);
+  }
+
   const response = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     temperature: 0,
-    messages,
+    messages: [systemMessage, ...messages],
     stream: true,
   });
 
